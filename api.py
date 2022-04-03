04f084e4e09101e558f5f473674f73b9ea46edd9
horia274 <ignat.horia.andrei@gmail.com>
2022-04-03 07:22:06 +0300

    selected roberta bot

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer
+from transformers import AutoModelForQuestionAnswering, AutoModelForMultipleChoice, AutoTokenizer
 
 from urls_getter import get_urls
 from scraper import get_urls_context
@@ -19,24 +19,35 @@ def sanity_check():
 @app.route('/question', methods=['POST'])
 def ask_me():
     req_data = request.get_json()
-    question = req_data['question_text']
+    question_text = req_data['question_text']
+    question_type = req_data['question_type']
+    answer_choices = req_data['answer_choices']
+    answer_type = req_data['answer_type']
 
     num_urls = 3
-    urls = get_urls(question, num_urls)
-    # for url in urls:
-    #     print(url)
+    urls = get_urls(question_text, num_urls)
 
     context = get_urls_context(urls)
-    # print(context)
 
+    # ~18
     model_name = 'deepset/roberta-base-squad2'
 
-    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+    # ~14
+    # model_name = 'deepset/bert-base-cased-squad2'
+
+    # ~ ; fara truncation setat; foarte lent
+    # model_name = 'deepset/bert-large-uncased-whole-word-masking-squad2'
+
+    # ~18; dureaza prea mult
+    # model_name = 'deepset/roberta-large-squad2'
+
+    dir_ans_model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+    # mult_ch_model = AutoModelForMultipleChoice.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    answer = find_answer(model, tokenizer, question, context)
+    answer = find_answer(dir_ans_model, tokenizer, question_text, question_type, answer_choices, answer_type, context)
 
-    print(question, '---', answer)
+    print(question_text, '---', answer)
 
     return jsonify({'answer': answer}), 200
 
--- processer.py
+++ processer.py
@@ -1,15 +1,45 @@
+import difflib
+import random
+import re
+
 from transformers import pipeline
-from scraper import get_url_context
+from util import text2int
 
-def find_answer(model, tokenizer, question, context):
-    # max_score = 0.0
-    # best_answer = ''
 
-    tokenizer.encode(question, truncation=True, padding=True)
-    nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+def find_answer(dir_ans_model, tokenizer, question_text, question_type, answer_choices, answer_type, context):
+    tokenizer.encode(question_text, truncation=True, padding=True)
+    nlp = pipeline('question-answering', model=dir_ans_model, tokenizer=tokenizer)
 
-    answer = nlp({'question': question, 'context': context})['answer']
-    return answer
+    answer = nlp({'question': question_text, 'context': context})['answer']
+
+    curr_answer = answer
+
+    if answer_type == 'numeric':
+        if answer[0].isalpha():
+            curr_answer = text2int(answer)
+        else:
+            answer = re.sub('[^0-9]', '', answer)
+            curr_answer = int(answer)
+
+    if question_type == 'multiple_choice':
+        curr_answer = str(curr_answer)
+        curr_poss_answers = curr_answer.split(' ')
+
+        for poss_answer in curr_poss_answers:
+            if poss_answer in answer_choices:
+                return poss_answer
+
+        final_choices = difflib.get_close_matches(curr_answer, answer_choices)
+
+        if len(final_choices) == 0:
+            print("!!!!!!!!!!!! RANDOM FINAL CHOICE !!!!!!!!!!!!!!!!")
+            final_choice = random.choice(answer_choices)
+        else:
+            final_choice = final_choices[0]
+
+        return final_choice
+
+    return curr_answer
 
     # for url in urls:
     #     context = get_url_context(url)
